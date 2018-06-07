44e9a3845f18132d11fb35c336dda9710129341a
Danni40 <dlewis2@live.maryville.edu>
2018-06-06 20:18:28 -0500

    wk5 exercise part 1 with explanation of error first callback

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var fs = require('fs');
+//^^this assigns the fs (file system) module in node to a variable
 
 fs.readFile('/foo.txt', function(err, data) {
-    // TODO: Error Handling
+    /*Error Handling and error first callback used here.  The file is not
+    available and should throw an error when ran in the console.  The concept
+    of error first callback is to take in 2 arguments and if the function
+    throws an error, the programmer is able to handle the error, and if not,
+    the code would proceed successfully.  In this case, the file does not
+    exist so it will throw and error when looking for the file in the directory.
+    */
     if(err) {
         console.log('Unknown Error');
         return;
       }
+      /*I have handled the error to return the string "Unknown Error" onto
+      the console when trying to read the non-existent file.  This is a
+      simple example of an error first callback*/
     console.log(data);
   });
